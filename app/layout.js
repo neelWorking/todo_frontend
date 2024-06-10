f3c3967404a4b5601d0b1c5409479bae4b1e2cda
neelworking <neelwoking@gmail.com>
2024-06-10 17:56:59 +0530

    add

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
-
+//
 export const metadata = {
   title: "Tasks-by | | Track and Organize your track",
   description: "Organize your task and monitor progress effortlessly!",
